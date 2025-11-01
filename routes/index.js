1100f578e88467a981e2b69b5ca4d00e3869c153
jkjorgie <jkjorgie@yahoo.com>
2025-11-01 07:21:53 -0600

    did some more work

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ router.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+router.use('/users', require('./users'));
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+        console.log(`Database is listening and node running on port ${port}`);
+      });
+  }
 });
+
+
 
 /*app.get('/', (req, res) => {
     res.send('Hello World');
